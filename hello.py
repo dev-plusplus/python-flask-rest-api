a55c6c4777a93e428c72b0ed01471e3d83999a9e
Angel Lacret <alacret@gmail.com>
2023-01-18 23:28:07 -0500

    mongodb and validation

--- hello.py
+++ hello.py
@@ -1,12 +1,30 @@
 from flask import Flask, request
+import pymongo
+from bson.objectid import ObjectId
+
+from cerberus import Validator
+
+schema = {"name": {"type": "string"}, "description": {"type": "string"}}
+validator: Validator = Validator(schema)
 
 app = Flask(__name__)
-tasks = []
 
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+# Connect to Mongo
+client = pymongo.MongoClient("mongodb+srv://demo:123demo123@cluster0.09pkckl.mongodb.net/?retryWrites=true&w=majority")
+task_database = client['task_database']
+task_collection = task_database['task_collection']
+print(client.server_info())
+
+
+def serialize(mongo_row):
+    mongo_row['_id'] = str(mongo_row['_id'])
+    return mongo_row
 
 
 # CRUD for Tasks
@@ -17,30 +35,36 @@ def hello():
 
 @app.route('/tasks', methods=['GET', 'POST'])
 def tasks_view():
-    global tasks
     if request.method == 'GET':
+        tasks_cursor = task_collection.find({})
+        tasks = []
+        for task in tasks_cursor:
+            tasks.append(serialize(task))
         return tasks
 
     if request.method == 'POST':
         data = request.get_json()
-        data['id'] = len(tasks)
-        tasks.append(data)
+        is_valid = validator.validate(data)
+        if is_valid is False:
+            return validator.errors
+        # insert_one modifies the input object to set the _id when you don't put an _id
+        task_collection.insert_one(data)
+        serialize(data)
         return data
 
 
 @app.route('/task/<task_id>', methods=['GET', 'PUT', 'DELETE'])
 def task_view(task_id):
-    global tasks
-    _task_id = int(task_id)
+    _task_id = ObjectId(task_id)
     if request.method == 'GET':
-        return tasks[_task_id]
+        task = task_collection.find_one({"_id": _task_id})
+        return serialize(task)
 
     if request.method == 'PUT':
         new_task = request.get_json()
-        old_task = tasks[_task_id]
-        old_task.update(new_task)
-        return old_task
+        task_collection.update_one({"_id": _task_id}, {"$set": new_task})
+        return new_task
 
     if request.method == 'DELETE':
-        del tasks[_task_id]
-        return 'OK!'
+        task_collection.delete_one({"_id": _task_id})
+        return {"response": True}
